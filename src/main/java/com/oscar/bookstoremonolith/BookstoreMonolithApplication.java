855ced85a38e31919b383767bcebe14acf87a816
olaguna7 <oscarlagunacaminero@gmail.com>
2025-07-15 16:36:26 +0200

    first commit

---
+++ src/main/java/com/oscar/bookstoremonolith/BookstoreMonolithApplication.java
@@ -0,0 +1,13 @@
+package com.oscar.bookstoremonolith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreMonolithApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookstoreMonolithApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/oscar/bookstoremonolith/BookstoreMonolithApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oscar.bookstoremonolith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreMonolithApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
